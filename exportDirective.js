38f8a343e4d1774dac3d547cdbddaaa63aa29b12
Joris de Keijser <chinook25@gmail.com>
2017-10-30 16:09:56 +0100

    Better svg exports

--- exportDirective.js
+++ exportDirective.js
@@ -31,18 +31,18 @@ define(['lodash', 'd3', 'jQuery'], function(_, d3, $) {
                   width: sourceImage.width,
                   height: sourceImage.height
                 });
-              var context = $canvasElement[0].getContext("2d");
+              var context = $canvasElement[0].getContext('2d');
               context.drawImage(sourceImage, 0, 0);
 
-              var a = document.createElement("a");
-              a.download = scope.fileName + ".png";
-              a.href = $canvasElement[0].toDataURL("image/png");
+              var a = document.createElement('a');
+              a.download = scope.fileName + '.png';
+              a.href = $canvasElement[0].toDataURL('image/png');
 
               // work around firefox security feature that stops triggering click event from script
-              var clickEvent = new MouseEvent("click", {
-                "view": window,
-                "bubbles": true,
-                "cancelable": false
+              var clickEvent = new MouseEvent('click', {
+                'view': window,
+                'bubbles': true,
+                'cancelable': false
               });
               a.dispatchEvent(clickEvent);
             }
@@ -53,26 +53,42 @@ define(['lodash', 'd3', 'jQuery'], function(_, d3, $) {
               $svgElement.find('.nv-point-clips').remove();
 
               //can't set svg instructions as image src directly
-              var $image = createImage($svgElement);
-              $image[0].setAttribute('crossOrigin', 'anonymous');
-              $image.on('load', _.partial(exportImage, $image[0]));
+              var image = createImage($svgElement);
+              image[0].setAttribute('crossOrigin', 'anonymous');
+              image.on('load', _.partial(exportImage, image[0]));
             }
 
             function createImage($svgElement) {
+              $svgElement.find('.nv-background')
+                .attr('fill', 'white');
+              $svgElement.find('.nv-axis path')
+                .attr('fill', 'none')
+                .attr('stroke', 'black')
+                ;
+              $svgElement.find('path.nv-line')
+                .attr('fill', 'none');
+              d3.selectAll('.nv-point')
+                .style('stroke-opacity', 0)
+                .style('fill-opacity', 0);
+               d3.selectAll('text')
+                .attr('font-family', 'sans-serif')
+                .style('font-size', '12px');
+
               var html = $svgElement
                 .attr('height', $svgElement.height())
                 .attr('width', $svgElement.width())
-                .attr("version", 1.1)
-                .attr("xmlns", "http://www.w3.org/2000/svg")
+                .attr('version', 1.1)
+                .attr('xmlns', 'http://www.w3.org/2000/svg')
                 .attr('crossOrigin', 'anonymous')
                 .parent()[0]
                 .innerHTML;
 
               var imgsrc = 'data:image/svg+xml;base64,' + btoa(html);
-              var $img = jQuery('<img />', {
+              var img = $('<img />', {
                 src: imgsrc
               });
-              return $img;
+
+              return img;
             }
           }
         };
